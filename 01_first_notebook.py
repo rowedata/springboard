53800722de1c973317d5f40efa454a57efc874ca
Benazir Rowe <benazir.rowe@gmail.com>
2022-10-18 10:19:20 -0600

    py version of the notebook

---
+++ 01_first_notebook.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# 
+# 
+# My <b>first </b> Jupyter notebook
+# 
+# <i>This is italic text </i>
+# 
+
+# In[13]:
+
+
+x=5
+y=6
+z=x+y
+
+
+# In[14]:
+
+
+print(z)
+
+
+# In[6]:
+
+
+get_ipython().run_cell_magic('time', '', 'x=4\ny=6\nprint(x+y)')
+
